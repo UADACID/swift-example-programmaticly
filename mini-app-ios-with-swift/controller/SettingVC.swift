c353b426e46eb7fcab5ec37bd0c75c41ab133872
Pratama <pratamasetya99@gmail.com>
2017-12-28 00:04:43 +0700

    first commit

---
+++ mini-app-ios-with-swift/controller/SettingVC.swift
@@ -0,0 +1,35 @@
+//
+//  SettingVC.swift
+//  mini-app-ios-with-swift
+//
+//  Created by pratama setya aji on 12/27/17.
+//  Copyright © 2017 pratama setya aji. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
